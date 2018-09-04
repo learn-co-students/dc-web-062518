d00ea748b12b9e58000db1efb916f2967a3952d3
github username <github email address>
2018-09-04 16:42:14 -0400

    sushi code challenge review

---
+++ 47-code-challenge-review-sushi/sushi-saga-client/src/components/MoreButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MoreButton = (props) => {
+    return <button onClick={props.moreSushi}>
+            More sushi!
+          </button>
+}
+
+export default MoreButton
